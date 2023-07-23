1e387b9f37f39b5f0bfa5e6a84cd4cf52afe9af9
Frolov1987 <116515964+Frolov1987@users.noreply.github.com>
2023-07-23 21:41:32 +0300

    Add app version

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My git app.");
         System.out.println("Demo application.");
+        System.out.println("Version 1.0");
     }
 }
