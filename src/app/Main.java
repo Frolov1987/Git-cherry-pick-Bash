159c217dddcabc842e9de691d6bb03a15a151e1d
Frolov1987 <116515964+Frolov1987@users.noreply.github.com>
2023-07-23 21:39:30 +0300

    Add app description

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,2 +1,8 @@
-package app;public class Main {
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My git app.");
+        System.out.println("Demo application.");
+    }
 }
